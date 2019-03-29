9b192167bf0304dba1497af0933cd4322044bf54
N4DD2 <didong2nhom4@gmail.com>
2019-03-29 14:52:13 +0700

    Home Screen

---
+++ app/src/main/java/com/example/admin/myapplication/FragmentNTD.java
@@ -0,0 +1,13 @@
+package com.example.admin.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FragmentNTD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_fragment_ntd);
+    }
+}

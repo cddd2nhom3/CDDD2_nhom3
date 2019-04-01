974aaba0906fb025b75a7424f0674954e303835e
Hoàng Văn Giang <37360394+hoangvangiang@users.noreply.github.com>
2019-04-01 11:08:32 +0700

    Add Firebase và code chức năng đăng ký người dùng by Hoang Van Giang

---
+++ app/src/main/java/com/example/admin/myapplication/listener/LoginListener.java
@@ -0,0 +1,10 @@
+package com.example.admin.myapplication.listener;
+
+/**
+ * Created by LynkMieu on 4/9/2017.
+ */
+
+public interface LoginListener {
+    void loginSuccess();
+    void loginFailure(String message);
+}
---
+++ app/src/main/java/com/example/admin/myapplication/view/SampleClass.java
@@ -0,0 +1,8 @@
+package com.example.admin.myapplication.view;
+
+/**
+ * Created by LynkMieu on 4/5/2017.
+ */
+
+public class SampleClass {
+}

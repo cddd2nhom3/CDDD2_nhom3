79380bf8cb3b29c50eca3e8f179be01a3ded79f3
N4DD2 <duyenpham.no@gmail.com>
2019-04-06 08:29:21 +0700

    chức năng đăng kí nhưng bị lỗi(Phạm Trọng Duyễn)

--- app/src/main/java/com/example/admin/myapplication/TuyenDung.java
+++ app/src/main/java/com/example/admin/myapplication/TuyenDung.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-
 import com.example.admin.myapplication.datamodels.NhaTuyenDung;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -15,14 +14,16 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 public class TuyenDung extends AppCompatActivity {
+    private static final String TAG = TuyenDung.class.getSimpleName();
     private EditText edttenDoanhNghiep , edtEmail , edtMatKhau , edtDiaChi , edtSoDienThoai;
     private Button btnSubMit;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tuyen_dung);
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         final DatabaseReference myRef = database.getReference("NhaTuyenDung");
-        setContentView(R.layout.activity_tuyen_dung);
         edttenDoanhNghiep = (EditText) findViewById(R.id.edtHoTen);
         edtEmail = (EditText) findViewById(R.id.edtEmail);
         edtMatKhau = (EditText) findViewById(R.id.edtPassword);
@@ -32,14 +33,14 @@ public class TuyenDung extends AppCompatActivity {
         btnSubMit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 String tenDoanhNghiep = edttenDoanhNghiep.getText().toString();
                 String email = edtEmail.getText().toString();
                 String matKhau = edtMatKhau.getText().toString();
                 String diaChi = edtDiaChi.getText().toString();
                 int soDienThoai = Integer.parseInt(edtSoDienThoai.getText().toString());
+                String userId = myRef.push().getKey();
                 NhaTuyenDung NTD = new NhaTuyenDung(tenDoanhNghiep,email,matKhau,diaChi,soDienThoai);
-                myRef.child("NhaTuyenDung").setValue(NTD);
+                myRef.child(userId).setValue(NTD);
             }
         });
         // Read from the database
@@ -49,13 +50,13 @@ public class TuyenDung extends AppCompatActivity {
                 // This method is called once with the initial value and again
                 // whenever data at this location is updated.
                 String value = dataSnapshot.getValue(String.class);
-                Log.d("TAG", "Value is: " + value);
+                Log.d(TAG, "Value is: " + value);
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
                 // Failed to read value
-                Log.w("TAG", "Failed to read value.", error.toException());
+                Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
     }
